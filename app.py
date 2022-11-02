1973dde06ad04eefe94148c1c74f16da7e98cdd8
lars <l.rook2002@hotmail.nl>
2022-11-02 23:18:15 +0100

    added new new changes

--- app.py
+++ app.py
@@ -1,14 +1,22 @@
-from flask import Flask, jsonify, request
-import time
+from flask import Flask, request
 
 app = Flask(__name__)
-@app.route("/fMotor", method=["POST"])
+
 
 #response
+@app.route("/", method=["POST"])
 def response():
+
     #query = dict(request.form)['query']
     #result = query + " " + time.ctime()
-    return jsonify({"response" : "HELLO"})
+    return {'response' : "HELLO"}
 
+
+@app.before_request
+def only_json():
+    if not request.is_json:
+        abort(400)
+
+        
 #if __name__ == "__main__":
 #   app.run(host="0.0.0.0",)
