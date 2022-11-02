63e0d595db29a76c21b7cbf5b3e45c302377ee58
lars <l.rook2002@hotmail.nl>
2022-11-02 21:22:44 +0100

    testing

--- app.py
+++ app.py
@@ -6,9 +6,9 @@ app = Flask(__name__)
 
 #response
 def response():
-    query = dict(request.form)['query']
-    result = query + " " + time.ctime()
-    return jsonify({"response" : result})
+    #query = dict(request.form)['query']
+    #result = query + " " + time.ctime()
+    return jsonify({"response" : "HELLO"})
 
     if __name__ == "__main__":
         app.run(host="0.0.0.0",)
