630de55123db6445b25b91b4466e40edc6ab2f79
lars <l.rook2002@hotmail.nl>
2022-11-03 10:13:21 +0100

    tested with new project

--- app.py
+++ app.py
@@ -1,10 +1,24 @@
-from flask import Flask
+from pickle import load
+
+import pandas as pd
+import shap
+from flask import Flask, request, abort
+from keras.saving.save import load_model
 
 app = Flask(__name__)
 
-@app.route("/", methods=['POST'])
-def predict():
-  return {'response': "HELLO!"}
 
-if __name__ == "__main__":
-  app.run()
+
+@app.route('/', methods=['POST'])
+def predict():
+    #json = request.get_json(force=True, silent=False, cache=True)
+
+    return {
+        'response': "HGHELLUU",
+    }
+
+
+#@app.before_request
+#def only_json():
+#    if not request.is_json:
+#        abort(400)
